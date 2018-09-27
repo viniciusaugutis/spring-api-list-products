ef45acd360ffbb0a9cbd57754d6d1bdf68406a0f
Vinícius Augutis <vinicius_augutis@hotmail.com>
2018-09-27 17:26:03 -0300

    initial commit and creating initial structure

---
+++ api-list-products/src/test/java/com/products/api/ApiListProductsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.products.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiListProductsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
