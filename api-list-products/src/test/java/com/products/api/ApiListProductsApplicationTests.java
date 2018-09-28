8a1fbf6f0acd62685758112f45b38b85100b0f5c
Vinícius Augutis <vinicius_augutis@hotmail.com>
2018-09-28 00:26:52 -0300

    correcting unit tests

--- api-list-products/src/test/java/com/products/api/ApiListProductsApplicationTests.java
+++ api-list-products/src/test/java/com/products/api/ApiListProductsApplicationTests.java
@@ -1,18 +1,14 @@
 package com.products.api;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.Stack;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -20,6 +16,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import com.products.api.model.Product;
 import com.products.api.repository.ProductRepository;
 import com.products.api.repository.filter.ProductFilter;
+import com.products.api.repository.projection.ProductSummary;
 import com.products.api.service.ProductService;
 
 @RunWith(SpringRunner.class)
@@ -47,7 +44,7 @@ public class ApiListProductsApplicationTests {
 		
 		productFilter.setStack(stacks);
 		
-		Page<Object> pageObject = productService.filterProduct(productFilter,
+		Page<ProductSummary> pageObject = productService.filterProduct(productFilter,
 				PageRequest.of(0, 10));
 		assertEquals(2, pageObject.getTotalElements());
 	}
@@ -58,19 +55,13 @@ public class ApiListProductsApplicationTests {
 		List<Long> targetMarkets = new ArrayList<Long>();
 		targetMarkets.add(3L);
 		
-		productFilter.setStack(targetMarkets);
+		productFilter.setTargetMarket(targetMarkets);
 		
-		Page<Object> pageObject = productService.filterProduct(productFilter,
+		Page<ProductSummary> pageObject = productService.filterProduct(productFilter,
 				PageRequest.of(0, 10));
-		assertEquals(2, pageObject.getTotalElements());
+		
+		
+		assertEquals(true, pageObject.getContent().get(0).getTargetMarket().contains("Lojista que não desejam possuir ecommerce"));
 	}
-
-	/*
-	@Test
-	public void testProductServiceMockito() {
-		Optional<Product> product = Optional.of(new Product(1L, "Produto 1", "Descrição do produto 1"));
-		when(productService.findById(1L)).thenReturn(product);
-		assertEquals(product.get(), productService.findById(1L).get());
-	}*/
 
 }
